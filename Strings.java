92d71e74d8d12b1c2fee3142cc8d142741838c66
corv1njano <56222588+corv1njano@users.noreply.github.com>
2024-08-11 03:12:59 +0200

    basic stuff 1

---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main(String[] arguments) {
+
+        // Strings sind Zeichenketten
+        String zeichenkette = "Hier steht irgendein Text"; // S von String groß schreiben!
+
+        System.out.println(zeichenkette);
+    }
+}
